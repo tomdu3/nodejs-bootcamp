902e126e674aad093104aa61413f37f255c1a78c
Tomislav Dukez <tomdu3@ymail.com>
2024-01-29 15:09:23 +0000

    Fix: index.js

--- intro/index.js
+++ intro/index.js
@@ -2,30 +2,24 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// app - GET request
-
-// app.get('/', function(request, response) {
-//    console.log(request);
-//    resp.send('Hello World')
-// })
-
-app.get('/', (req, resp) => {
-    // resp.send('Hello World');
-    resp.sendFile(__dirname + '/index.html');
+app.get('/', (req, res) => {
+    // Serve an HTML file when a GET request is made to '/'
+    res.sendFile(__dirname + '/index.html');
     console.log(__dirname);
 });
 
 app.post('/', (req, res) => {
-    var num1 = Number(req.body.n1);
-    var num2 = Number(req.body.n2);
-    var result = num1 + num2;
+    console.log(req.body);
+    const num1 = Number(req.body.n1);
+    const num2 = Number(req.body.n2);
+    const result = num1 + num2;
     console.log(result);
     res.send('Your result is: ' + result);
-})
-// app.listen(3000); - we need to add the callback function
+});
 
 app.listen(3000, function() {
     console.log('Server is running on port 3000');
 });
-
